a1d229b4979a5afe20021d7f27cfd16e2e4f5944
我不是矿神 <39237997+tbc0309@users.noreply.github.com>
2023-05-31 16:51:50 +0800

    synology build

--- build-all.sh
+++ build-all.sh
@@ -4,17 +4,6 @@ PLATFORMS=(
   'linux/amd64'
   'linux/arm64'
   'linux/arm7'
-  'linux/arm5'
-  'linux/386'
-  'windows/amd64'
-  'windows/386'
-  'darwin/amd64'
-  'darwin/arm64'
-  'freebsd/amd64'
-  'linux/mips'
-  'linux/mipsle'
-  'linux/mips64'
-  'linux/mips64le'
 )
 
 type setopt >/dev/null 2>&1
@@ -71,40 +60,9 @@ for PLATFORM in "${PLATFORMS[@]}"; do
   set_gomips "$GOARCH"
   BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}${GOARM}"
   if [[ "${GOOS}" == "windows" ]]; then BIN_FILENAME="${BIN_FILENAME}.exe"; fi
-  CMD="GOOS=${GOOS} GOARCH=${GOARCH} ${GO_ARM} ${GO_MIPS} ${GOBIN} build ${BUILD_FLAGS} -o ${BIN_FILENAME} ./cmd"
+  CMD="CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} ${GO_ARM} ${GO_MIPS} ${GOBIN} build ${BUILD_FLAGS} -o ${BIN_FILENAME} ./cmd"
   echo "${CMD}"
   eval "$CMD" || FAILURES="${FAILURES} ${GOOS}/${GOARCH}${GOARM}"
-#  CMD="../upx -q ${BIN_FILENAME}"; # upx --brute produce much smaller binaries
-#  echo "compress with ${CMD}"
-#  eval "$CMD"
-done
-
-#####################################
-### Android build section
-#####
-
-declare -a COMPILERS=(
-  "arm7:armv7a-linux-androideabi21-clang"
-  "arm64:aarch64-linux-android21-clang"
-  "386:i686-linux-android21-clang"
-  "amd64:x86_64-linux-android21-clang"
-)
-
-export NDK_VERSION="25.2.9519653" # 25.1.8937393
-#export NDK_TOOLCHAIN=${ANDROID_HOME}/ndk/${NDK_VERSION}/toolchains/llvm/prebuilt/darwin-x86_64
-export NDK_TOOLCHAIN="${PWD}/../android-ndk-r25c/toolchains/llvm/prebuilt/linux-x86_64"
-GOOS=android
-
-for V in "${COMPILERS[@]}"; do
-  GOARCH=${V%:*}
-  COMPILER=${V#*:}
-  export CC="$NDK_TOOLCHAIN/bin/$COMPILER"
-  export CXX="$NDK_TOOLCHAIN/bin/$COMPILER++"
-  set_goarm "$GOARCH"
-  BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}${GOARM}"
-  CMD="GOOS=${GOOS} GOARCH=${GOARCH} ${GO_ARM} CGO_ENABLED=1 ${GOBIN} build ${BUILD_FLAGS} -o ${BIN_FILENAME} ./cmd"
-  echo "${CMD}"
-  eval "${CMD}" || FAILURES="${FAILURES} ${GOOS}/${GOARCH}${GOARM}"
 #  CMD="../upx -q ${BIN_FILENAME}"; # upx --brute produce much smaller binaries
 #  echo "compress with ${CMD}"
 #  eval "$CMD"
